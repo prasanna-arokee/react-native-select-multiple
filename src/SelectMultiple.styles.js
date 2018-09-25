5a6f0b3e6b94f5366a6bb531cb30e39390f4ea64
prasanna-arokee <35933900+prasanna-arokee@users.noreply.github.com>
2018-09-25 11:35:00 +0530

    Update SelectMultiple.styles.js

--- src/SelectMultiple.styles.js
+++ src/SelectMultiple.styles.js
@@ -4,16 +4,20 @@ export default StyleSheet.create({
   row: {
     flex: 1,
     flexDirection: 'row',
-    alignItems: 'center',
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
+    paddingVertical:15,
+    alignContent:'space-between',
+    justifyContent:'space-between',
+    paddingHorizontal:5
   },
   checkbox: {
-    width: 24,
-    height: 24,
-    marginRight: 5
+    width: 32,
+    height: 32,
+    alignContent:'flex-end',
   },
-  label: {}
+  label: {
+    fontSize:18,
+    fontFamily:'OpenSans-Bold',
+    color:'#000000',
+  }
 })
